8448c03b69b9ae163f34718ed6ba6bf7a6bd3f9a
sumit-vishwakarma <sumit.vishwakarma@averybit.in>
2023-06-13 14:34:14 +0530

    Added Edit, Delete, Add User model and search functionality

--- js/users.js
+++ js/users.js
@@ -2,17 +2,25 @@
 const array = [
     {
         id: 1,
+        email: "raftar@averybit.in",
         name: "Raftar",
     },
     {
         id: 2,
+        email: "raftar@averybit.in",
         name: "Honey",
 
     },
     {
         id: 3,
+        email: "raftar@averybit.in",
         name: "Mustang",
     },
+    {
+        id: 3,
+        email: "raftar@averybit.in",
+        name: "Mustang",
+    }
 ]
 
 let html = `
@@ -20,28 +28,30 @@ let html = `
     <div class="container d-flex justify-content-between">
         <h3>Users</h3>
         <div class="input-group w-50">
-            <input type="text" class="form-control" placeholder="Search User" aria-label="Search User" aria-describedby="button-addon2">
-            <button class="btn btn-outline-primary" type="button" id="button-addon2">Search</button>
+            <input id="searchValue" onkeyup="searchUser()" type="text" class="form-control" placeholder="Search User" aria-label="Search User" aria-describedby="button-addon2">
+            <button class="btn btn-outline-primary" type="button" id="searchUser">Search</button>
         </div>
-        <td><button type="button" class="btn btn-primary">Add User</button></td>
+        <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUsersModel">Add User</button></td>
     </div>
     <table class="table">
         <thead>
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
+                <th scope="col">Email</th>
                 <th scope="col">Edit</th>
                 <th scope="col">Delete</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody id="tableBody">
             ${
                 array.map((usr)=>{
                    return `<tr>
                         <th scope="row">${usr.id}</th>
                         <td>${usr.name}</td>
-                        <td><button type="button" class="btn btn-primary">Edit</button></td>
-                        <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
+                        <td>${usr.email}</td>
+                        <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editUserModel">Edit</button></td>
+                        <td><button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteUserModel">Delete</button></td>
                     </tr>`
                 })
             }
@@ -49,6 +59,8 @@ let html = `
     </table>
 </div>
 `
+
+// User Screen Manipulation
 // Getting screen element for change
 let screenContent = document.getElementById("screenContent");
 
@@ -59,3 +71,26 @@ users.addEventListener("click", () => {
     // changing content on click
     screenContent.innerHTML = html;
 })
+
+// Search functionality
+const searchUser = () => {
+    var value = document.getElementById("searchValue").value;
+
+    let tableBody = document.getElementById("tableBody");
+
+    let filteredUser = array.filter((item)=>{
+        return item.name.toLowerCase().includes(value);
+    })
+
+    let innerRows = filteredUser.map((usr)=>{
+        return `<tr>
+                    <th scope="row">${usr.id}</th>
+                    <td>${usr.name}</td>
+                    <td>${usr.email}</td>
+                    <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editUserModel">Edit</button></td>
+                    <td><button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteUserModel">Delete</button></td>
+                </tr>`
+    })
+
+    tableBody.innerHTML = innerRows;
+}
