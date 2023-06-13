5b3b058efd5be6baaf066f4865036251424c96a0
pawan29-06 <pawan.sahu@averybit.in>
2023-06-13 18:23:33 +0530

    added form for add meeting

--- js/script.js
+++ js/script.js
@@ -29,10 +29,133 @@ function toggleDropdown() {
   dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
 }
 
+// Function to toggle dropdown for add meeting
+
+$(document).ready(function() {
+  $("#myBtn").click(function() {
+    $("#myModal").modal();
+  });
+});
+
+
+// Data array for upcoming meetings
+var upcomingMeetingData = [];
+
+// Get the table body reference
+var roomDataBody = document.getElementById("roomData");
+
+function generateRows() {
+  roomDataBody.innerHTML = ""; // Clear existing rows
+
+  // Generate the table rows dynamically
+  upcomingMeetingData.forEach(function(data) {
+    var row = document.createElement("tr");
+
+    var noCell = document.createElement("td");
+    noCell.textContent = data.no;
+    row.appendChild(noCell);
+
+    var meetingTitleCell = document.createElement("td");
+    meetingTitleCell.textContent = data.meetingTitle;
+    row.appendChild(meetingTitleCell);
+
+    var roomCell = document.createElement("td");
+    roomCell.textContent = data.room;
+    row.appendChild(roomCell);
+
+    var clockInTimeCell = document.createElement("td");
+    clockInTimeCell.textContent = data.clockInTime;
+    row.appendChild(clockInTimeCell);
+
+    var clockOutTimeCell = document.createElement("td");
+    clockOutTimeCell.textContent = data.clockOutTime;
+    row.appendChild(clockOutTimeCell);
+
+    var actionCell = document.createElement("td");
+    var editButton = document.createElement("button");
+    editButton.setAttribute("type", "button");
+    editButton.setAttribute("class", "edit-button");
+    editButton.setAttribute("id", "editBtn");
+    editButton.textContent = "Edit";
+
+    editButton.addEventListener("click", function() {
+      $("#editModal").modal("show");
+      // You can add logic here to pre-fill the modal with the room data for editing
+    });
+
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "delete-button");
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener("click", function() {
+      $("#deleteRoomModal").modal("show");
+      // You can add logic here to pre-fill the modal with the room data for editing
+    });
+
+    actionCell.appendChild(editButton);
+    actionCell.appendChild(deleteButton);
+    row.appendChild(actionCell);
+
+    roomDataBody.appendChild(row);
+  });
+}
+
+// Load the data from local storage if available
+var upcomingMeetingData = JSON.parse(localStorage.getItem("upcomingMeetingData")) || [];
+
+// Function to save the data to local storage
+function saveDataToLocalStorage() {
+  localStorage.setItem("upcomingMeetingData", JSON.stringify(upcomingMeetingData));
+}
+
+// Handle form submission to add a new room
+var addRoomForm = document.getElementById("addRoomForm");
+addRoomForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var meetingTitleInput = document.getElementById("meetingTitle");
+  var roomInput = document.getElementById("roomType");
+  var clockInTimeInput = document.getElementById("clockInTime");
+  var clockOutTimeInput = document.getElementById("clockOutTime");
+
+  var meetingTitle = meetingTitleInput.value;
+  var room = roomInput.value;
+  var clockInTime = clockInTimeInput.value;
+  var clockOutTime = clockOutTimeInput.value;
+
+  // Generate a unique ID for the new room
+  var serialNumber = upcomingMeetingData.length + 1;
+
+  // Add the new room to the dummyData array
+  upcomingMeetingData.push({
+    no: serialNumber,
+    meetingTitle: meetingTitle,
+    room: room,
+    clockInTime: clockInTime,
+    clockOutTime: clockOutTime
+  });
+
+
+  // Save the data to local storage
+  saveDataToLocalStorage();
+
+  // Clear the form inputs
+  meetingTitleInput.value = "";
+  roomInput.value = "";
+  clockInTimeInput.value = "";
+  clockOutTimeInput.value = "";
+
+  // Generate the updated table rows
+  generateRows();
+
+  // Hide the modal
+  $("#myModal").modal("hide");
+});
+
+generateRows();
 
 
 
 
 
 
- 
