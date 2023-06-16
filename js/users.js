9c6d60dc121ed2e546f9d19327648b31ba5c7245
sumit-vishwakarma <sumit.vishwakarma@averybit.in>
2023-06-16 11:23:59 +0530

    edit user fuctionality

--- js/users.js
+++ js/users.js
@@ -25,9 +25,9 @@ const generateUsersScreen = (usersArray) => {
         <div>
         </div>
         ${
-            usersArray ?
+            usersArray.length > 0 ?
         `<div style="overflow: auto">
-        <table class="table table-bordered mt-2">
+        <table class="table table-bordered mt-4">
             <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -44,7 +44,7 @@ const generateUsersScreen = (usersArray) => {
                             <th scope="row">${usr.id}</th>
                             <td>${usr.username}</td>
                             <td>${usr.email}</td>
-                            <td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editUserModel">Edit</button></td>
+                            <td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editUserModel" onclick="editUserFunction(${usr.id})">Edit</button></td>
                             <td><button type="button" class="btn btn-outline-danger" onclick="deleteUser(${usr.id})">Delete</button></td>
                         </tr>`
                     })
@@ -116,6 +116,7 @@ let submitUser = () => {
     }
 }
 
+// Delete user
 const deleteUser = (userId) => {
     localStorage.setItem("userId", userId);
     if (confirm("Are you sure you want to delete this user?")) {
@@ -126,6 +127,48 @@ const deleteUser = (userId) => {
             users.splice(userIndex, 1);
             localStorage.setItem("users", JSON.stringify(users));
             alert("User deleted successfully");
+        } else {
+            alert("User not found");
+        }
+        location.reload();
+    }
+}
+
+// Edit User Model Open
+const editUserFunction = (editUserId) => {
+    localStorage.setItem("editUserId", editUserId);
+    console.log(editUserId)
+
+    let users = JSON.parse(localStorage.getItem('users'));
+    const user = users.find((user) => Number(user.id) === editUserId);
+    console.log(user)
+
+    document.getElementById("editUsername").value = user.username;
+    document.getElementById("editEmail").value = user.email;
+    document.getElementById("editPassword").value = user.password;
+
+    $('#editUserModel').modal('show');
+}
+
+
+// Edit user
+const editUser = () => {
+    let username = document.getElementById("editUsername").value;
+    let email = document.getElementById("editEmail").value;
+    let password = document.getElementById("editPassword").value;
+
+    const editUserId = localStorage.getItem("editUserId");
+    if (confirm("Are you sure you want to edit this user?")) {
+        let users = JSON.parse(localStorage.getItem('users'));
+        const userIndex = users.findIndex((user) => user.id === editUserId);
+
+        if (userIndex !== -1) {
+            users[userIndex].username = username;
+            users[userIndex].email = email;
+            users[userIndex].password = password;
+
+            localStorage.setItem("users", JSON.stringify(users));
+            alert("User edited successfully");
         } else {
             alert("User not found");
         }
