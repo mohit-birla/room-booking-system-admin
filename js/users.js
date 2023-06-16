eee316ce6a48e5bef1bb99d7a239b8b8dc4da968
sumit-vishwakarma <sumit.vishwakarma@averybit.in>
2023-06-16 12:57:50 +0530

    search fuctionality

--- js/users.js
+++ js/users.js
@@ -9,48 +9,46 @@ usersButton.addEventListener("click", () => {
 
 const generateUsersScreen = (usersArray) => {
     let usersScreen = document.getElementById("screenContent");
-    let usersHtml = `
-    <div class="w-75 m-3 p-3">
+    let usersHtml = 
+    `<div class="w-75 m-3 p-3">
         <div class="input-group d-flex d-sm-none w-100 mb-2">
-            <input id="searchValue" onkeyup="searchUser()" type="text" class="  form-control" placeholder="Search User" aria-label="Search User" aria-describedby="button-addon2">
+            <input type="text" class="  form-control" placeholder="Search User" aria-label="Search User" aria-describedby="button-addon2">
         </div>
         <div class="container d-flex justify-content-between m-0">
             <h3>Users</h3>
             <div class="input-group d-none d-md-flex w-50">
-                <input onkeyup="searchUser()" type="text" id="searchValue"  class="form-control" placeholder="Search User" aria-label="Search User" aria-describedby="button-addon2">
-                <button class="btn btn-outline-info" type="button" id="searchUser">Search</button>
+                <input type="text" id="searchValue" oninput="searchUser()" class="form-control" placeholder="Search User" aria-label="Search User" aria-describedby="button-addon2">
+                <!-- <button class="btn btn-outline-info"   type="button" id="searchUser">Search</button> -->
             </div>
             <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#addUsersModel">Add User</button>
-        </div>
-        <div>
         </div>
         ${
             usersArray ?
         `<div style="overflow: auto">
-        <table class="table table-bordered mt-4">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
-                </tr>
-            </thead>
-            <tbody id="tableBody">
-                ${
-                    usersArray.map((usr)=>{
-                    return `<tr>
-                            <th scope="row">${usr.id}</th>
-                            <td>${usr.username}</td>
-                            <td>${usr.email}</td>
-                            <td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editUserModel" onclick="editUserFunction(${usr.id})">Edit</button></td>
-                            <td><button type="button" class="btn btn-outline-danger" onclick="deleteUser(${usr.id})">Delete</button></td>
-                        </tr>`
-                    })
-                }
-            </tbody>
-        </table>
+            <table class="table table-bordered mt-4">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody">
+                    ${
+                        usersArray?.map((usr)=>{
+                        return `<tr>
+                                <th scope="row">${usr.id}</th>
+                                <td>${usr.username}</td>
+                                <td>${usr.email}</td>
+                                <td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editUserModel" onclick="editUserFunction(${usr.id})">Edit</button></td>
+                                <td><button type="button" class="btn btn-outline-danger" onclick="deleteUser(${usr.id})">Delete</button></td>
+                            </tr>`
+                        })
+                    }
+                </tbody>
+            </table>
         </div>` : `<div style="text-align: center; margin-top: 10rem"><h5>No Users</h5></div>`
         } 
     </div>
@@ -61,30 +59,33 @@ const generateUsersScreen = (usersArray) => {
 
 
 // Search functionality
-// const searchUser = () => {
-//     var value = document.getElementById("searchValue").value
-//     console.log(value)
+const searchUser = () => {
+    var value = document.getElementById("searchValue").value
+    let tableBody = document.getElementById("tableBody");
 
-//     let tableBody = document.getElementById("tableBody");
+    let filteredUser = usersArray.filter((item)=>{
+        return item.email.toLowerCase().includes(value);
+    })
 
-//     let filteredUser = usersArray.filter((item)=>{
-//         return item.email.toLowerCase().includes(value);
-//     })
-
-//     console.log(filteredUser)
-
-//     let innerRows = filteredUser.map((usr)=>{
-//         return `<tr>
-//                     <th scope="row">${usr.id}</th>
-//                     <td>${usr.name}</td>
-//                     <td>${usr.email}</td>
-//                     <td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editUserModel">Edit</button></td>
-//                     <td><button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteUserModel">Delete</button></td>
-//                 </tr>`
-//     })
-
-//     tableBody.innerHTML = innerRows;
-// }
+    var innerRows ='';
+    const generateFilteredUser = () => {
+        innerRows = filteredUser?.map((usr)=>{
+            return `<tr>
+                        <th scope="row">${usr.id}</th>
+                        <td>${usr.username}</td>
+                        <td>${usr.email}</td>
+                        <td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editUserModel">Edit</button></td>
+                        <td><button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteUserModel">Delete</button></td>
+                    </tr>`
+        })
+    }
+    generateFilteredUser()
+    if (innerRows.length > 0) {
+        tableBody.innerHTML = innerRows.join('');
+    } else {
+        tableBody.innerHTML = "";
+    }
+}
 
 // save data to localStorage
 // Add Users
